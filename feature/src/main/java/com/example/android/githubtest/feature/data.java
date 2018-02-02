2bafc68f6655eca92ea2e04ea4aaf13d3abce751
florinacimpan12 <florinacimpan16@gmail.com>
2018-02-02 21:37:35 +0200

    schimbari mockup

---
+++ feature/src/main/java/com/example/android/githubtest/feature/data.java
@@ -0,0 +1,14 @@
+package com.example.android.githubtest.feature;
+
+/**
+ * Created by admin on 02/02/2018.
+ */
+
+public class data {
+    static int x;
+    public data(){}
+
+    public void data(){
+        x++;
+    }
+}
